6dcf8898f1a04fc0233ee6dc4a987f9313df7555
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-10-18 01:49:42 +0300

    add TestOpenHelper

---
+++ app/src/main/java/com/example/q2/TestOpenHelper.java
@@ -0,0 +1,61 @@
+package com.example.q2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class TestOpenHelper extends SQLiteOpenHelper {
+
+    // データーベースのバージョン
+    private static final int DATABASE_VERSION = 1;
+
+    // データーベース名
+    private static final String DATABASE_NAME = "TestDB.db";
+    private static final String TABLE_NAME = "testdb";
+    private static final String _ID = "_id";
+    private static final String COLUMN_NAME_TITLE = "company";
+    private static final String COLUMN_NAME_SUBTITLE = "stockprice";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_TITLE + " TEXT," +
+                    COLUMN_NAME_SUBTITLE + " INTEGER)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+
+    TestOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // テーブル作成
+        // SQLiteファイルがなければSQLiteファイルが作成される
+        db.execSQL(
+                SQL_CREATE_ENTRIES
+        );
+
+        Log.d("debug", "onCreate(SQLiteDatabase db)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // アップデートの判別
+        db.execSQL(
+                SQL_DELETE_ENTRIES
+        );
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+}
