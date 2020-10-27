c7129d9a861cac8cd65b4b2085ae4881ee41cf16
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-10-27 22:04:52 +0200

    simplify

--- app/src/main/java/com/example/q2/MainActivity.java
+++ app/src/main/java/com/example/q2/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
         //Ad Place
         // Test App ID
