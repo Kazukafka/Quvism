81df556a804ea779da20d40ec27189c5e18aff20
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-11-02 19:59:57 +0200

    \n textView in StartAc

--- app/src/main/java/com/example/q2/FlipActivity.java
+++ app/src/main/java/com/example/q2/FlipActivity.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FlipActivity extends AppCompatActivity {
 
@@ -18,9 +20,64 @@ public class FlipActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip);
         final ImageView imageView = findViewById(R.id.flip2);
+        final TextView textView = findViewById(R.id.flipTest);
+
+        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(imageView, "scaleX", 1f, 0f);
+        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(imageView, "scaleX", 0f, 1f);
+        oa1.setInterpolator(new DecelerateInterpolator());
+        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        final Button button = findViewById(R.id.bbb);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                if (a == 1){
+                    textView.setText("ABCD");
+                    a = 2;
+                } else {
+                    textView.setText("LLLLLLLLLLLLLLL");
+                }
+                oa1.setInterpolator(new DecelerateInterpolator());
+                oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                oa1.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        imageView.setImageResource(R.drawable.test_c_image);
+                        oa2.start();
+                    }
+                });
+                oa1.start();
+            }
+        });
+        /*
         imageView.setOnClickListener(new View.OnClickListener() {
+            int a = 1;
+            @Override
+            public void onClick(View v) {
+                if (a == 1){
+                    textView.setText("ABCD");
+                    a = 2;
+                } else {
+                    textView.setText("LLLLLLLLLLLLLLL");
+                }
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+
+                int a = 1;
+                if (a == 1){
+                    textView.setText("ABCD");
+                    a = 2;
+                } else {
+                    textView.setText("LLLLLLLLLLLLLLL");
+                }
+
                 final ObjectAnimator oa1 = ObjectAnimator.ofFloat(imageView, "scaleX", 1f, 0f);
                 final ObjectAnimator oa2 = ObjectAnimator.ofFloat(imageView, "scaleX", 0f, 1f);
                 oa1.setInterpolator(new DecelerateInterpolator());
@@ -37,5 +94,7 @@ public class FlipActivity extends AppCompatActivity {
                 oa1.start();
             }
         });
+
+         */
     }
 }
