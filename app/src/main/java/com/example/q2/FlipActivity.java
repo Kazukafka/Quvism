749767e247fed8ce80a5e92e3e75dcd8db83dc50
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-11-03 13:49:46 +0200

    flip and text Clip is ok

--- app/src/main/java/com/example/q2/FlipActivity.java
+++ app/src/main/java/com/example/q2/FlipActivity.java
@@ -2,7 +2,10 @@ package com.example.q2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FlipActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FlipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip);
+
+        TextView textView = findViewById(R.id.txtFlip);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("KKKKKKKKKKK");
+            }
+        });
     }
 }
